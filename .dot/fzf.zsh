908b138feb43716b5aa33249894bce86c39ddd83
Zhang <zhang.bo14@zte.com.cn>
2018-03-17 14:03:25 +0800

    update fzf zsh

--- .dot/fzf.zsh
+++ .dot/fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/ricd/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/ricd/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/ricd/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/ricd/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
