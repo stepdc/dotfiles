5c27207cbc36fe1660fd90595791225ac5f4f2d2
stepdc <habwtl@foxmail.com>
2019-04-27 15:05:07 +0800

    update

--- .dot/tools.zsh
+++ .dot/tools.zsh
@@ -1,6 +1,10 @@
 # alias
 alias k=kubectl
+alias ks='kubectl -n kube-system'
 alias mk=minikube
+alias mkl='minikube service list'
+alias mkt='minikube -p tbw'
+alias mktl='minikube -p tbw service list'
 alias h=helm
 
 [[ -s $HOME/.dot/zsh_completion.d/_minikube ]] && \
